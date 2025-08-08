ef80ffd0a74a3674c8fb872330310d5f1f02d4a2
moreira <kauanm0reiratti@gmail.com>
2025-08-08 16:45:16 +0000

    k

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+#include <string.h>
  int main(){
-
+ 
  printf("=====================================\n");
   printf("Desafio Super Trunfo. \n");
   printf("by: Kauan Moreira. \n");
@@ -9,21 +9,19 @@
 
 
  //Sequencia da primeira carta
- char estado1, numero1[02], cidade1[50];
+ char estado1, numero1[20], cidade1[50];
  int População1,Pontos_turisticos1;
  float Pib1;
 
  //Sequencia da segunda carta
- char estado2, numero2[02], cidade2[50];
+ char estado2, numero2[20], cidade2[50];
  int População2,Pontos_turisticos2;
  float Pib2;
 
 
  //Primeira Carta
- printf("Primeira Carta \n");
-
  printf("Digite o estado (A-H): \n");
- scanf("%s", &estado1);
+ scanf("%c", &estado1);
 
  printf("Digite um numero que representa o estado (01-04) \n");
  scanf("%s", &numero1);
@@ -32,15 +30,20 @@
  scanf("%s", &cidade1);
 
  printf("Digite a População da cidade escolhida: \n");
- scanf("%d", &População1), 
+ scanf("%i", &População1), 
 
-  printf("Digite o PIB da cidade escolhida: \n");
-  scanf("%f", &Pib1);
+ printf("Digite o PIB da cidade escolhida: ");
+ scanf("%f", &Pib1);
+
+
+
+
+
 
 
 
  // Segunda Carta
-
+ 
 
 
 
